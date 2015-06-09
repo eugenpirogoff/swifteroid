6c92438fa70ec33ab925986cee36a4e7744d8366
Eugen Pirogoff <eugenpirogoff@me.com>
2015-06-09 17:39:50 +0200

    - added VideoPreviewMode

--- Swifteroid/Swifteroid/ViewController.swift
+++ Swifteroid/Swifteroid/ViewController.swift
@@ -14,9 +14,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var recButton: UIButton!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var deleteButton: UIButton!
+
+    let session = AVCaptureSession()
+    var backCameraDevice : AVCaptureDevice!
+    var frontCameraDevice : AVCaptureDevice!
+    var output : AVCaptureStillImageOutput!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configurateCamera()
     }
     
     override func didReceiveMemoryWarning() {
@@ -26,11 +32,41 @@ class ViewController: UIViewController {
     
     @IBAction func recTouchDown(sender: AnyObject) {
         print("Exposure Started")
-        
     }
     
     @IBAction func recTouchUp(sender: AnyObject) {
         println("Exposure Ended")
     }
+    
+    func configurateCamera(){
+        let availableCameraDevices = AVCaptureDevice.devicesWithMediaType(AVMediaTypeVideo)
+        
+        for device in availableCameraDevices as! [AVCaptureDevice] {
+            if device.position == .Back {
+                backCameraDevice = device
+            }
+            else if device.position == .Front {
+                frontCameraDevice = device
+            }
+        }
+        
+        var error:NSError?
+        
+        let possibleCameraInput: AnyObject? = AVCaptureDeviceInput.deviceInputWithDevice(backCameraDevice, error: &error)
+        
+        if let backCameraInput = possibleCameraInput as? AVCaptureDeviceInput {
+            if session.canAddInput(backCameraInput) {
+                session.addInput(backCameraInput)
+            }
+        }
+        
+        var previewLayer = AVCaptureVideoPreviewLayer.layerWithSession(session) as! AVCaptureVideoPreviewLayer
+        previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+        previewLayer.frame = imageView.bounds
+        imageView.layer.addSublayer(previewLayer)
+        session.sessionPreset = AVCaptureSessionPresetPhoto
+        session.startRunning()
+    }
+    
 }
 
